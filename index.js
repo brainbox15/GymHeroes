ff674a31e9d3159c4bca74474e6344f9b05b994e
MICK <omorakafearless@yahoo.com>
2021-11-12 07:22:36 -0800

    added a new file

---
+++ index.js
@@ -0,0 +1,6 @@
+menuIcon = document.getElementById('menu-icon')
+Navigate = document.getElementById('navigate')
+menuIcon.addEventListener('click', function () {
+
+    Navigate.classList.toggle('show')
+})
