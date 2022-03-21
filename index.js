9d69ac5617472c4e9d3b3a68c5a4b20c66e170fa
MICK <omorakafearless@yahoo.com>
2022-03-21 07:34:01 -0700

    add a README to the project

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-menuIcon = document.getElementById('menu-icon')
-Navigate = document.getElementById('navigate')
+// grabs the element by id
+var menuIcon = document.getElementById('menu-icon')
+
+// grabs the element by id
+var navigate = document.getElementById('navigate')
+
+// attaches a listener to menuIcon
 menuIcon.addEventListener('click', function () {
 
-    Navigate.classList.toggle('show')
+    navigate.classList.toggle('show')
 })
